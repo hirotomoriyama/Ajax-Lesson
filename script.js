5d078f3318058909f12aa792851774997d8f94e1
hirotomoriyama-g <hiroto.moriyama7204@gmail.com>
2021-04-01 22:39:07 +0900

    Ajaxを用いて天気の取得

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-$('#tab-contents .tab[id!="tab1"]').hide();
+API_KEY = "e9ed4456c442789a141071d2373c2ee4"
 
-$('#tab-menu a').on('click',function(event){
-  $("#tab-contents .tab").hide();
-  $("#tab-menu .active").removeClass("active");
-  $(this).addClass("active");
-  $($(this).attr("href")).show();
-  event.preventDefault();
+$(function(){
+  $('#btn').on('click',function(){
+    $.ajax({
+      url:"http://api.openweathermap.org/data/2.5/weather?q="+$('#cityname').val()+"&units=metric&appid="+API_KEY,
+      dataType:'jsonp',
+    }).done(function(data){
+
+      $('#place').text(data.name);
+      $('#temp_max').text(data.main.temp_max);
+      $('#temp_min').text(data.main.temp_min);
+      $('#humidity').text(data.main.humidity);
+      $('#speed').text(data.wind.speed);
+      $('#weather').text(data.weather[0].main);
+      $('img').attr("src","http://openweathermap.org/img/w/"+data.weather[0].icon+".png");
+      $('img').attr("alt",data.weather[0].main);
+
+    }).fail(function(data){
+
+      alert('通信に失敗しました。');
+
+    });
+  });
 });
-
-e9ed4456c442789a141071d2373c2ee4
